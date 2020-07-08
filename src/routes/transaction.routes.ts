a885a730f07b960fa703bb9da2bb93c5b18f9715
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-07-08 09:36:09 -0400

    :art: Create routes and return values

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const allTransactions = transactionsRepository.all();
+    const balanceTransactions = transactionsRepository.getBalance();
+
+    const resultTransactions = {
+      transactions: [...allTransactions],
+      balance: balanceTransactions,
+    };
+
+    return response.json(resultTransactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +25,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transactionCreated = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transactionCreated);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
